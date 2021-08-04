16df9cbba22596966c18d726401ad92c84d3760b
Tony Brown <tb148@tuta.io>
2021-08-04 10:40:38 +0800

    Fix bug

--- anti.py
+++ anti.py
@@ -11,7 +11,7 @@ form["req_password"] = os.getenv("TBGS_PASSWORD")
 form["login"] = "Login"
 tbgs.post(url + "login.php?action=in", data=form)
 form = {"delete": "Delete"}
-for i in range(568733, 569356):
+for i in range(568708, 568733):
     cnt += 1
     tbgs.post(url + "delete.php?id=" + str(i), data=form)
     print("Post {} deleted.".format(i))
