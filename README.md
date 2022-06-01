8dc045703906f43968f0d113355ee71b9aef6fa7
Gilbert189 <gilbertdlo@gmail.com>
2022-06-01 16:40:34 +0700

    initial changes
    i forgot this exists on my phone
    not sure if it works but whatever

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# postbot
-A post bot for the TBGs.
+# postqueuer
+A simple post queuer bot for the TBGs.
 
-**WARNING: This bot is for demonstration purposes only. Using this bot might cause you being banned for violating rule 2g!**
+Oh, would you look a that. A bot that's used to break rules has now transformed into a useful post queuer.
+
+Uses [celery](https://github.com/celery/celery) to queue posts. You might need to read the repo.
+
+Once run, postqueuer will use these environment variables:
+```
+TBGS_USERNAME: The bot's username
+TBGS_PASSWORD: The bot's password
+QUEUER_BROKER: The queuer's message broker (the place to send commands)
+QUEUER_BACKEND: The queuer's backend (where the result of commands get sent in)
+```
--- bot.py
+++ bot.py
@@ -1,18 +1,54 @@
+"""A simple post queuer for the TBGs."""
+
+
 import os
 import requests
 import time
+from celery import Celery
+from bs4 import BeautifulSoup as bs 
 
-tbgs = requests.Session()
-url = "https://tbgforums.com/forums/"
-cnt = 0
-form = {"form_sent": "1"}
-form["req_username"] = os.getenv("TBGS_USERNAME")
-form["req_password"] = os.getenv("TBGS_PASSWORD")
-form["login"] = "Login"
-tbgs.post(url + "login.php?action=in", data=form)
-form = {"form_sent": "1"}
-while True:
-    cnt += 1
-    form["req_message"] = os.getenv("TBGS_MESSAGES")
-    tbgs.post(url + "post.php?tid=" + os.getenv("TBGS_TOPICTID"), data=form)
-    print("Post {} sent.".format(cnt))
+
+if sys.argv[0].endswith("celery"): # ensures this code is run only when using celery
+    # Not sure why Tony doesn't store these on variables.
+    # Is this a security thing or is it just Tony being lazy?
+    USERNAME = os.getenv("TBGS_USERNAME")
+    PASSWORD = os.getenv("TBGS_PASSWORD")
+
+    tbgs = requests.Session()
+    url = "https://tbgforums.com/forums/"
+
+    # Login to the TBGs
+    form = {"form_sent": "1"}
+    form["req_username"] = USERNAME
+    form["req_password"] = PASSWORD
+
+    form["login"] = "Login"
+    tbgs.post(f"{url}login.php?action=in", data=form)
+
+
+# Buy some celery
+BROKER = os.getenv("QUEUER_BROKER")
+BACKEND = os.getenv("QUEUER_BACKEND")
+queuer = Celery("postqueuer", backend=BACKEND, broker=BROKER)
+
+
+@queuer.task
+def post(msg: str, topic_id: int) -> tuple:
+    global tbgs
+    
+    # Make sure the tbgs variable exists
+    if "tbgs" not in globals():
+        raise RuntimeError("Can't run this function locally")
+    
+    form = {"form_sent": "1"}
+    form["req_message"] = msg
+    res = tbgs.post(f"{url}post.php?tid={topic_id}", data=form)
+    
+    # Check for errors
+    tree = bs(res.text)
+    error = tree.find(id="posterror")
+    if error:
+        error = error.findall("strong")
+        error = [x.text for x in error]
+        raise RuntimeError(error)
+    return {"status": res.status_code, "content": res.content}
